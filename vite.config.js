619356b487003f9a9e72db02f01025636ce4d7ac
Victor <vincitore.code@gmail.com>
2025-06-15 21:34:24 -0600

    configure for gitpages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-web/', 
   plugins: [react(), tailwindcss()],
 })
